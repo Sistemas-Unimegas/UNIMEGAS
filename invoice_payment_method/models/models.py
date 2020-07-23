99ea25833243e8dbbdf289b2273e3ef29fa9c275
Unimegas <sistemas@unimegas.com>
2020-07-23 00:29:54 -0600

    Update models.py

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -5,5 +5,5 @@ class Datos_facturas(models.Model):
 
 	_inherit = 'account.move'
 
-	tipo_factura=fields.Selection([('PUE','Pago en una sola exhibición'),
-	                           ('PPD','Pago en parcialidades o diferido')])
+	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
+	                           ('PPD','PPD - Pago en parcialidades o diferido')])
