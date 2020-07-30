29000e1961456b879a16b1e9baa914dfba178156
Unimegas <sistemas@unimegas.com>
2020-07-29 22:03:40 -0600

    Update Product.py

--- pos_retail/models/product/Product.py
+++ pos_retail/models/product/Product.py
@@ -9,14 +9,14 @@ _logger = logging.getLogger(__name__)
 
 class pos_sale_extra(models.Model):
     _name = "pos.sale.extra"
-    _description = "Sale extra items base on core product"
+    _description = "Venta de artículos adicionales basados ​​en producto principal"
 
-    product_tmpl_id = fields.Many2one('product.template', 'Base Product', required=1,
+    product_tmpl_id = fields.Many2one('product.template', 'Producto Base', required=1,
                                       domain=[('available_in_pos', '=', True)])
-    product_id = fields.Many2one('product.product', 'Product extra', required=1,
+    product_id = fields.Many2one('product.product', 'Producto extra', required=1,
                                  domain=[('available_in_pos', '=', True)])
-    quantity = fields.Float('Default Qty', default=1, required=1)
-    list_price = fields.Float('List Price', required=1)
+    quantity = fields.Float('Cantidad predeterminada', default=1, required=1)
+    list_price = fields.Float('Precio de Lista', required=1)
 
     @api.model
     @api.onchange('product_id')
@@ -27,102 +27,102 @@ class pos_sale_extra(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    pos_combo_item_ids = fields.One2many('pos.combo.item', 'product_combo_id', string='Combo Items')
+    pos_combo_item_ids = fields.One2many('pos.combo.item', 'product_combo_id', string='Productos de Combo')
     is_combo = fields.Boolean(
-        'Combo Bundle/Pack',
-        help='Active it and see to tab Combo/Pack and adding Items for Combo Future'
+        'Paquete de Combo / Paquete',
+        help='Actívelo y vea la pestaña Combo / Paquete y agregue elementos para la opción de Combo'
     )
     is_combo_item = fields.Boolean(
-        'Dynamic Combo Item',
-        help='Allow this product become item combo of Another Product'
+        'Elemento de Combo dinámico',
+        help='Permitir que este producto se convierta en elemento combinado de otro producto'
     )
     combo_limit = fields.Integer(
-        'Combo Item Limit',
-        help='Limit combo items can allow cashier add / combo')
+        'Límite de artículo de Combo',
+        help='Los elementos combinados de límite pueden permitir agregar / combo')
     is_credit = fields.Boolean('Is Credit', default=False)
-    multi_category = fields.Boolean('Multi Category')
+    multi_category = fields.Boolean('Multi Categoría')
     pos_categ_ids = fields.Many2many(
         'pos.category',
-        string='POS Multi Category')
-    multi_uom = fields.Boolean('Multi Unit')
+        string='POS Multi Categoría')
+    multi_uom = fields.Boolean('Multi Unidad')
     price_uom_ids = fields.One2many(
         'product.uom.price',
         'product_tmpl_id',
-        string='Price by Sale Unit')
-    multi_variant = fields.Boolean('Product Multi variant')
+        string='Precio por unidad de venta')
+    multi_variant = fields.Boolean('Producto Multi variante')
     pos_variant_ids = fields.One2many(
         'product.variant',
         'product_tmpl_id',
-        string='Multi Variants')
-    cross_selling = fields.Boolean('Cross Selling')
+        string='Multi Variantes')
+    cross_selling = fields.Boolean('Venta cruzada')
     cross_ids = fields.One2many(
         'product.cross',
         'product_tmpl_id',
-        string='Cross Selling Items')
+        string='Artículos de venta cruzada')
     supplier_barcode = fields.Char(
-        'Supplier Barcode', copy=False,
-        help="Supplier Barcode Product, You can Input here and scan on POS")
+        'Código de barras del proveedor', copy=False,
+        help="Producto de código de barras del proveedor, puede ingresar aquí y escanear en POS")
     barcode_ids = fields.One2many(
         'product.barcode',
         'product_tmpl_id',
-        string='Multi Barcode')
+        string='Código de barras múltiple')
     pack_ids = fields.One2many(
         'product.quantity.pack',
         'product_tmpl_id',
-        string='Quantities Pack')
-    pos_sequence = fields.Integer('POS Sequence')
-    is_voucher = fields.Boolean('Is Voucher', default=0)
-    sale_extra = fields.Boolean(string='Active Sale Extra')
+        string='Paquete de cantidades')
+    pos_sequence = fields.Integer('POS Secuencia')
+    is_voucher = fields.Boolean('Es Voucher', default=0)
+    sale_extra = fields.Boolean(string='Venta activa extra')
     sale_extra_item_ids = fields.One2many(
         'pos.sale.extra',
         'product_tmpl_id',
-        'Sale Extra Items')
-    minimum_list_price = fields.Float('Min Sales Price', default=0)
-    sale_with_package = fields.Boolean('Sale with Package')
-    price_unit_each_qty = fields.Boolean('Active Sale Price each Quantity')
+        'Venta de artículos extra')
+    minimum_list_price = fields.Float('Precio de venta mínimo', default=0)
+    sale_with_package = fields.Boolean('Venta con empaque')
+    price_unit_each_qty = fields.Boolean('Precio de venta activo cada cantidad')
     product_price_quantity_ids = fields.One2many(
         'product.price.quantity',
         'product_tmpl_id',
-        'Price each Quantity')
-    qty_warning_out_stock = fields.Float('Qty Warning out of Stock', default=10)
+        'Precio cada cantidad')
+    qty_warning_out_stock = fields.Float('Cantidad - Advertencia fuera de stock', default=10)
     combo_price = fields.Float(
-        'Combo Item Price',
-        help='This Price will replace public price and include to Line in Cart'
+        'Precio del producto en Combo',
+        help='Este precio reemplazará el precio público e incluirá una línea de venta en el POS'
     )
     combo_limit_ids = fields.One2many(
         'pos.combo.limit',
         'product_tmpl_id',
-        'Combo Limited Items by Category'
+        'Artículos en Combo limitados por categoría'
     )
     name_second = fields.Char(
-        'Second Name',
-        help='If you need print pos receipt Arabic,Chinese...language\n'
-             'Input your language here, and go to pos active Second Language')
-    special_name = fields.Char('Special Name')
-    uom_ids = fields.Many2many('uom.uom', string='Units the same category', compute='_get_uoms_the_same_category')
+        'Segundo Nombre',
+        help='Si necesita imprimir recibo POS en árabe, chino ... idioma\n'
+             'Ingrese su idioma aquí, y vaya a Pos Segundo lenguaje activo')
+    special_name = fields.Char('Nombre Especial')
+    uom_ids = fields.Many2many('uom.uom', string='Unidades de la misma categoría', compute='_get_uoms_the_same_category')
     note_ids = fields.Many2many(
         'pos.note',
         'product_template_note_rel',
         'product_tmpl_id',
         'note_id',
-        string='Notes Fixed'
+        string='Notas fijas'
     )
     tag_ids = fields.Many2many(
         'pos.tag',
         'product_template_tag_rel',
         'product_tmpl_id',
         'tag_id',
-        string='Tags'
+        string='Etiquetas'
     )
-    pos_branch_id = fields.Many2one('pos.branch', string='Branch')
+    pos_branch_id = fields.Many2one('pos.branch', string='Sucursal')
     commission_rate = fields.Float(
-        'Commission Rate',
+        'Porcentaje de comision',
         default=50,
-        help='Commission Rate (%) for sellers'
+        help='Tasa de comisión (%) para vendedores'
     )
     cycle = fields.Integer(
-        'Cycle',
-        help='Total cycle times, customer can use in Spa Business'
+        'Ciclo',
+        help='Tiempos de ciclo totales que el cliente puede usar en Negocios Spa'
     )
 
     def add_barcode(self):
